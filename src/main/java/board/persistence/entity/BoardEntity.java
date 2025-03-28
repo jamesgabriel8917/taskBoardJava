a4cea16189d00a8effd6397cb98eadb424bf69cf
James Gabriel <jamesgabriel8917@gmail.com>
2025-03-27 22:04:37 -0300

    continuacao no desenvolvimento do projeto com a inclusao do BoardColumnDAO e seus respectivos servicos

--- src/main/java/board/persistence/entity/BoardEntity.java
+++ src/main/java/board/persistence/entity/BoardEntity.java
@@ -2,6 +2,7 @@ package board.persistence.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +12,7 @@ public class BoardEntity {
 
     private Long id;
     private String name;
+    @ToString.Exclude
     private List<BoardColumnEntity> boardColumns = new ArrayList<>();
 
 
